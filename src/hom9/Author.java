6596fc4d7f832386feb0381cdb2584c6211de883
am999 <am9999072080@gmail.com>
2023-04-03 22:18:44 +0300

    Commit1

--- src/hom9/Author.java
+++ src/hom9/Author.java
@@ -1,4 +1,51 @@
 package hom9;
 
+import java.util.Objects;
+
 public class Author {
+    private String name;
+    private String surname;
+    private String middleName;
+
+    @Override
+    public String toString() {
+        return name + '\'' +
+                surname + '\'' +
+                middleName + '\'';
+    }
+
+    public Author(String name, String surname, String middleName) {
+        this.name = name;
+        this.surname = surname;
+        this.middleName = middleName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author)) return false;
+        Author author = (Author) o;
+        return name.equals(author.name) && surname.equals(author.surname) && middleName.equals(author.middleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, middleName);
+
+    }
+
+    public String getMiddleName() {
+        return middleName;
+//    }
+//    public String toString(){
+//        return  this.name+" "+this.surname+" "+this.middleName;
+    }
 }
